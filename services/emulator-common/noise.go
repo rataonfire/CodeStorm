98a16dc476166f9391aef587d75c4ee7446a6a0c
Victoria <Vika.mustakimova@yandex.ru>
2026-05-11 18:32:03 +0300

    vibecoding

---
+++ services/emulator-common/noise.go
@@ -0,0 +1,63 @@
+package emulatorcommon
+
+import (
+	"math/rand"
+	"time"
+)
+
+type NoiseConfig struct {
+	MissingPct     float64
+	DuplicatePct   float64
+	WrongAmountPct float64
+	WrongFeePct    float64
+	DelayMinMs     int
+	DelayMaxMs     int
+}
+
+func ApplyNoise(event *PaymentTransactionEvent, cfg *NoiseConfig) (shouldSend bool, shouldDuplicate bool) {
+	shouldSend = true
+	shouldDuplicate = false
+
+	// Пропуск
+	if rand.Float64() < cfg.MissingPct {
+		return false, false
+	}
+
+	// Искажение суммы
+	if rand.Float64() < cfg.WrongAmountPct {
+		event.AmountMinor = mutateAmount(event.AmountMinor)
+	}
+
+	// Искажение комиссии
+	if rand.Float64() < cfg.WrongFeePct {
+		event.FeeMinor = mutateFee(event.FeeMinor)
+	}
+
+	// Дубль
+	if rand.Float64() < cfg.DuplicatePct {
+		shouldDuplicate = true
+	}
+
+	// Задержка
+	if cfg.DelayMaxMs > 0 {
+		delayMs := cfg.DelayMinMs
+		if cfg.DelayMaxMs > cfg.DelayMinMs {
+			delayMs += rand.Intn(cfg.DelayMaxMs - cfg.DelayMinMs)
+		}
+		time.Sleep(time.Duration(delayMs) * time.Millisecond)
+	}
+
+	return shouldSend, shouldDuplicate
+}
+
+func mutateAmount(amount int64) int64 {
+	// +/- 10%
+	factor := 0.9 + rand.Float64()*0.2
+	return int64(float64(amount) * factor)
+}
+
+func mutateFee(fee int64) int64 {
+	// +/- 50%
+	factor := 0.5 + rand.Float64()
+	return int64(float64(fee) * factor)
+}
